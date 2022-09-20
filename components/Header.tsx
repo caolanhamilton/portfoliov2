98aa23499a571e89d824fc57ef0fba8ec116ef2c
Caolan <caolanhamilton75@gmail.com>
2022-09-20 10:17:48 +0100

    Create header with social icons

---
+++ components/Header.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { SocialIcon } from "react-social-icons";
+import { motion } from "framer-motion";
+
+type Props = {};
+
+export default function header({}: Props) {
+  return (
+    <header className="sticky top-0 flex items-start max-w-6xl mx-auto z-1">
+      <motion.div
+        className="flex flex-row items-center"
+        initial={{
+          x: -500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1,
+        }}
+      >
+        {/* social icon */}
+        <SocialIcon
+          url="https://www.linkedin.com/in/caolanhamilton/"
+          fgColor="white"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="https://github.com/caolanhamilton"
+          fgColor="white"
+          bgColor="transparent"
+        ></SocialIcon>
+        <SocialIcon
+          className="cursor-pointer"
+          network="email"
+          fgColor="white"
+          bgColor="transparent"
+        />
+      </motion.div>
+    </header>
+  );
+}
